7721be3f5a851a37c194a7e1331d34536dbdf7a3
Muhammadjon Muxamedov <muhammadjon.200415@gmail.com>
2023-06-19 10:33:07 +0500

    added programming language model

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -1,1 +1,12 @@
-# Register your models here.
+from django.contrib import admin
+
+from apps.common.models import ProgrammingLanguage
+
+
+@admin.register(ProgrammingLanguage)
+class ProgrammingLanguageAdmin(admin.ModelAdmin):
+    list_display = ("name", "short_name", "is_active")
+    list_editable = ("is_active",)
+    list_filter = ("is_active",)
+    search_fields = ("name", "short_name")
+    prepopulated_fields = {"short_name": ("name",)}
