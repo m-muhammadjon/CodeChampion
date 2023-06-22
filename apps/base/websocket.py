23cbc46842f08eabf539beeba3e6f07e5de30af1
Muhammadjon Muxamedov <muhammadjon.200415@gmail.com>
2023-06-22 23:52:58 +0500

    added websocket to attempts list but not finished

--- apps/base/websocket.py
+++ apps/base/websocket.py
@@ -2,9 +2,9 @@ from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 
 
-def send_attempt_info_to_group(user_id: int, created: bool, attempt_id: int, status: str, time: int, memory: int,
-                               is_finished: bool
-                               ) -> None:
+def send_attempt_info_to_group(
+    user_id: int, created: bool, attempt_id: int, status: str, time: int, memory: int, is_finished: bool
+) -> None:
     layer = get_channel_layer()
     for group_name, method in [("attempt", "attempt.message"), (f"{user_id}", "user_submission_info")]:
         async_to_sync(layer.group_send)(
@@ -12,6 +12,7 @@ def send_attempt_info_to_group(user_id: int, created: bool, attempt_id: int, sta
             {
                 "type": method,
                 "created": created,
+                "user_id": user_id,
                 "attempt_id": attempt_id,
                 "status": status,
                 "time": time,
--- staticfiles/assets/js/socket.js
+++ staticfiles/assets/js/socket.js
@@ -1,4 +1,10 @@
-const ws_url = `ws://${window.location.host}/ws/user-socket/${user_id}`;
+console.log(window.location.pathname);
+if (window.location.pathname.match(/\/problems\/(\d+)\/attempts/)) {
+    let ws_url = `ws://${window.location.host}/ws/user-socket/${user_id}`;
+
+} else if (window.location.pathname === "/attempts") {
+    let ws_url = `ws://${window.location.host}/ws/attempt-socket`;
+}
 const socket = new WebSocket(ws_url);
 
 socket.onopen = function (e) {
@@ -9,11 +15,30 @@ socket.onmessage = function (e) {
     console.log("Socket message received.");
     const data = JSON.parse(e.data);
     let attempt = `tr#attempt-${data["attempt_id"]}`;
+    if (data["created"] === true) {
+        // $(`<tr id="attempt-${data['attempt_id']}">
+        //     <td class="attempt-id">
+        //         ${request_user_id === data['user_id'] ? `<a href="/attempts/${data['attempt_id']}" class="blue">${'attempt_id'}</a>` : data['attempt_id']}
+        //     </td>
+        //     <td class="attempt-user">
+        //         <a href="" class="blue-hover">USER</a>
+        //     </td>
+        //     <td class="attempt-problem">
+        //         <a href="" class="blue-hover">PROBLEM</a></td>
+        //     <td class="attempt-language">LANGUAGE</td>
+        //     <td class="attempt-verdict text-dark">
+        //         Waiting
+        //     </td>
+        //     <td class="attempt-time">0 ms</td>
+        //     <td class="attempt-memory">0 KB</td>
+        //     <td class="attempt-created">${data['created']}</td>
+        // </tr>`.insertAfter("thead"));
+    }
+
     if (data["is_finished"] === true) {
         document.querySelector(`${attempt} td.attempt-verdict`).innerHTML = data["status"];
         document.querySelector(`${attempt} td.attempt-time`).innerHTML = `${data["time"]} ms`;
         $(`${attempt} td.attempt-verdict`).attr("class", data["status"] === "Accepted" ? "accepted" : "wrong");
-
     } else {
         document.querySelector(`${attempt} td.attempt-verdict`).innerHTML = data["status"];
     }
